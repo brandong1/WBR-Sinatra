6fdcd0ffd71bfbb3638812865d15c35fc0c8ea58
brandong1 <brandon@brandonleeproductions.com>
2019-02-02 23:33:09 -0700

    create user class

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+    has_many :liquors
+    has_secure_password
+    validates :username, uniqueness :true
+    #can also be written as validates_uniqueness_of :username? or is this syntax rails?
+
+    def slug
+        username.downcase.gsub(" ","-")
+    end
+
+    def self.find_by_slug(slug)
+        User.find {|user| user.slug == slug }
+    end
+end
