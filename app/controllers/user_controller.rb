42aa2ad2465f75dbc3c96e023ebbe0419b27ef78
brandong1 <brandon@brandonleeproductions.com>
2019-02-03 09:58:44 -0700

    complete signup up

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,10 @@
 class UserController < ApplicationController
     
+    get '/signup' do 
+        if logged_in?
+            redirect :'/views/liquors'
+        else
+            redirect :'/signup'
+        end
+    end
 end
