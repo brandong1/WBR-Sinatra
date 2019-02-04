5ed9cf59b4f3edcfa0b0241944a584e0c8d3fc2e
brandong1 <brandon@brandonleeproductions.com>
2019-02-03 22:42:18 -0700

    define login, signup, user slug and logout routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,4 +7,20 @@ class UserController < ApplicationController
             redirect :'/signup'
         end
     end
+
+    post '/signup' do 
+        #if username and pw are not blank, create user, save user, redirect to liquor index
+    end
+
+    get '/login' do
+    end
+
+    post '/login' do
+    end
+
+    get '/user/:slug' do
+    end
+
+    get '/logout' do
+    end
 end
