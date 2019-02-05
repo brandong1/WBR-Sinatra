01d27fb609d862b3affb8f6f3e9d80829fcc01fa
brandong1 <brandon@brandonleeproductions.com>
2019-02-04 22:14:41 -0700

    complete post signup route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,6 +10,17 @@ class UserController < ApplicationController
 
     post '/signup' do 
         #if username and pw are not blank, create user, save user, redirect to liquor index
+        if !params[:username].empty? && !params[:password].empty?
+            @user = User.new(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
+            if @user.save
+                session[:user_id] = @user.id
+                redirect :'/liquors'
+            else
+                redirect '/' #flash message here?
+            end
+        else
+            redirect '/'
+        end
     end
 
     get '/login' do
@@ -21,6 +32,7 @@ class UserController < ApplicationController
     end
 
     post '/login' do
+        
     end
 
     get '/user/:slug' do
