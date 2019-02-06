366692f2b3ed27281d45a487520461a899953182
brandong1 <brandon@brandonleeproductions.com>
2019-02-05 18:52:50 -0700

    update post login route syntax

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -35,7 +35,7 @@ class UserController < ApplicationController
         @user = User.find_by(username: params[:username])
         if @user && @user.authenicate(params[:password])
             session[:user_id] = @user.id
-            redirect :'/wines'
+            redirect :'/liquors'
         else
             redirect :'/'
         end
