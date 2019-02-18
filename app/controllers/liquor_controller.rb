efc502d2715f69e6c512ba0e0916b7d87070311f
brandong1 <brandon@brandonleeproductions.com>
2019-02-17 19:56:24 -0700

    create /liquors/new controller action

--- app/controllers/liquor_controller.rb
+++ app/controllers/liquor_controller.rb
@@ -1,11 +1,28 @@
 class LiquorController < ApplicationController
     
-get '/liquors' do
-    if logged_in?
-        @user = current_user
-        @liquors = Liquor.all
-        erb :'liquors/liquors'
-    else
-        redirect '/'
+    get '/liquors' do
+        if logged_in?
+            @user = current_user
+            @liquors = Liquor.all
+            erb :'liquors/liquors'
+        else
+            redirect '/'
+        end
     end
+
+    get '/liquors/new' do
+        if logged_in?
+            @user = current_user
+            erb :'liquors/new'
+        else
+            redirect '/'
+        end
+    end
+
+
+
+
+
+
+
 end
