29e38287c47f43b95a1c821ad4455f3181073d2c
brandong1 <brandon@brandonleeproductions.com>
2019-02-19 20:41:46 -0700

    add post '/liquors' controller action

--- app/controllers/liquor_controller.rb
+++ app/controllers/liquor_controller.rb
@@ -9,5 +9,16 @@ class LiquorController < ApplicationController
         erb :'/liquors/new'
     end
 
-    
+    # get '/liquors/:slug' do
+    #     @song = Liquor.find_by_slug(params[:slug])
+    #     erb :'/liquors/show'
+    # end
+
+    post '/liquors' do
+        @liquors = Liquor.create(name: params['Name'])
+        @liquor.user = User.find_or_create_by(name: params['User Name'])
+        @liquor.save
+    end
+
+
 end
