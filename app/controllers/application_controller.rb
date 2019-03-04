c84129f0e970763c00276c5285edf4c1ff2cdfe6
brandong1 <brandon@brandonleeproductions.com>
2019-03-03 21:49:42 -0700

    fix current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,11 @@ class ApplicationController < Sinatra::Base
   
   helpers do
     def current_user
-      User.find(session[:user_id])
+      session[:user_id]
     end
     
     def logged_in?
       User.find(session[:user_id])
     end
   end
-
 end
