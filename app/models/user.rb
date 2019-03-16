99e8c0cb5ef8a47c0a8bd2986fb9823623206e99
brandong1 <brandon@brandonleeproductions.com>
2019-03-16 11:28:01 -0600

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
     has_many :liquors
     has_secure_password
+    
+    validates :username, presence: true
+    validates :email, presence: true
     validates :username, uniqueness: :true
+    validates :email, uniqueness: true
     #can this also be written as validates_uniqueness_of :username? or is this syntax rails?
 
     def slug
