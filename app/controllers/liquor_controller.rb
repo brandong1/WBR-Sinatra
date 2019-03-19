783aebc134df60b5e6771cc9b56db0f6ea7c5deb
brandong1 <brandon@brandonleeproductions.com>
2019-03-18 23:07:33 -0600

    create liquor edit and delete routes

--- app/controllers/liquor_controller.rb
+++ app/controllers/liquor_controller.rb
@@ -7,10 +7,10 @@ class LiquorController < ApplicationController
 
     get '/liquors/new' do
         if logged_in?
-        erb :'/liquors/new'
+            erb :'/liquors/new'
         else
             redirect '/'
-    end
+        end
     end
 
     # get '/liquors/:slug' do
@@ -28,7 +28,7 @@ class LiquorController < ApplicationController
         if  !params[:name].empty? &&
             !params[:description].empty? &&
             !params[:price].empty?
-                @user = User.find_by_id(session[:user_id])
+            @user = User.find_by_id(session[:user_id])
             Liquor.create(name: params[:name], description: params[:description], price: params[:price], user_id: @user.id)
             @liquors = current_user.liquors
                         #binding.pry
@@ -38,6 +38,34 @@ class LiquorController < ApplicationController
         end
     end
 
+    get '/liquors/:id/edit' do
+        if logged_in?
+            @liquors = Liquor.find_by_id(params[:id])     
+            if @liquors && @liquors.user == current_user
+                erb :'/liquors/edit'
+            end
+        else
+            redirect '/'
+        end
+    end
+    
+    patch '/liquors/:id/edit' do 
+        # @liquors.update(name: params[:name]...)
+    end
+
+    get '/liquors/:id/delete' do
+    if logged_in?
+        @liquors = Liquor.find_by_id(params[:id])
+            if @liquors && @liquors.user == current_user
+                erb :'liquors/delete'
+            else
+                redirect to '/users/show'
+            end
+        else
+                redirect '/'
+        
+        end
+    end
     delete '/liquors/:id' do
         if logged_in?
           @user = current_user
@@ -52,6 +80,4 @@ class LiquorController < ApplicationController
           redirect back
         end
     end
-
-
 end
