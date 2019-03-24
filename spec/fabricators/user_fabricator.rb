a0a0ac77041d6eece01634fb7e8999ee5a943b6a
brandong1 <brandon@brandonleeproductions.com>
2019-03-24 11:18:42 -0600

    delete old test db. create new db

--- spec/fabricators/user_fabricator.rb
+++
@@ -1,6 +0,0 @@
-Fabricator(:user) do
-    liquors(count: 2)
-    name {Faker::Name.name}
-    username {Faker::Internet.username}
-    email {Faker::Internet.free_email #=> "freddy@gmail.com"}
-    password {Faker::Internet.password #=> "Vg5mSvY1UeRg7"}
